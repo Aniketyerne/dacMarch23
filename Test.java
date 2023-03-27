2fc1bccdbc22c4d9bf10a252d43c2799d507765a
Aniketyerne <128138536+Aniketyerne@users.noreply.github.com>
2023-03-27 23:41:40 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,17 @@
+/*OUTPUT:-
+F:\CPOS\Assignment\Aniket Yerne\Assignment1>javac Test.java
+
+F:\CPOS\Assignment\Aniket Yerne\Assignment1>java Test
+Hello...
+Hear Aniket Yerne
+*/
+class Test{
+public static void main(String args[])
+{
+ System.out.println("Hello...");
+ System.out.println("Hear Aniket Yerne");
+ 
+	 
+}
+
+}
